a36b6827140c859d2c08180e49ff465236687fa9
RufusDerrid <rufusderrid@gmail.com>
2017-11-23 15:41:04 +0300

    init

---
+++ Start.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace console 
+{
+    public class Startup 
+    {
+        public Startup(IHostingEnvironment env) 
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment environment, 
+            ILoggerFactory loggerFactory) 
+        {
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("Test");
+            });
+        }
+
+    }
+}
